c5bd1b6844b09921309fe17b6c9cb887b272278b
AlexBo2019NN <52696086+AlexBo2019NN@users.noreply.github.com>
2020-12-15 17:18:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ Created on Sat Dec 12 17:06:31 2020
 @author: Alex Bocharov
 """
 # -*- coding: utf-8 -*-
-token ='1442551341:AAGW2DPtlFDHQlS-d7QUfAdtXWoT7sCUnLo'
-#token = '1323959039:AAEoPJ8PjXkYm596k_WWVuUeZ6XFucXA0vs'  second bot
+token ='TOKEN_OF_YOUR_BOT'
