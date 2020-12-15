aade7b478bd0ad789e6137b41462dc2292f4c06b
AlexBo2019NN <52696086+AlexBo2019NN@users.noreply.github.com>
2020-12-15 17:17:49 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 17:06:31 2020
+
+@author: Alex Bocharov
+"""
+# -*- coding: utf-8 -*-
+token ='1442551341:AAGW2DPtlFDHQlS-d7QUfAdtXWoT7sCUnLo'
+#token = '1323959039:AAEoPJ8PjXkYm596k_WWVuUeZ6XFucXA0vs'  second bot
